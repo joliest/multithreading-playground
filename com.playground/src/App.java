54cb74eac8b257f5aa53ac055ee4a6c1a8316a3b
Joliver Estampador <joliverestampador@gmail.com>
2023-06-13 07:46:45 +0800

    extending Thread.class and adding Threads.sleep()

--- com.playground/src/App.java
+++ com.playground/src/App.java
@@ -1,38 +1,39 @@
-class Runner1 implements Runnable {
+class Runner1 extends Thread {
     @Override
     public void run() {
-        for (int i = 0; i < 10; ++i)
+        for (int i = 0; i < 10; ++i) {
+            try {
+                // this thread will sleep at 1s, making the execution slower
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("Runner1: " + i);
+        }
 
     }
 }
 
-class Runner2 implements Runnable{
+class Runner2 extends Thread {
     @Override
     public void run() {
-        for (int i = 0; i < 10; ++i)
+        for (int i = 0; i < 10; ++i) {
+            try {
+                // this thread will sleep at 1s
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             System.out.println("Runner2: " + i);
+        }
     }
 }
 
 public class App {
     public static void main(String[] args) {
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10; ++i)
-                    System.out.println("Runner1: " + i);
-
-            }
-        });
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i < 10; ++i)
-                    System.out.println("Runner2: " + i);
-
-            }
-        });
+        Thread t1 = new Runner1();
+        Thread t2 = new Runner2();
 
         // achieving multi threading
         // not a parallel execution
