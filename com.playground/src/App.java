581bfef2d28bba25ead09ce8462849562eef2f98
Joliver Estampador <joliverestampador@gmail.com>
2023-06-13 07:31:21 +0800

    using anonymous class (same results)

--- com.playground/src/App.java
+++ com.playground/src/App.java
@@ -17,8 +17,22 @@ class Runner2 implements Runnable{
 
 public class App {
     public static void main(String[] args) {
-        Thread t1 = new Thread(new Runner1());
-        Thread t2 = new Thread(new Runner2());
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; ++i)
+                    System.out.println("Runner1: " + i);
+
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; ++i)
+                    System.out.println("Runner2: " + i);
+
+            }
+        });
 
         // achieving multi threading
         // not a parallel execution
