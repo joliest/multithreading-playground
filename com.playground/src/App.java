693edf96b54172280de14bf021874999224425ba
Joliver Estampador <joliverestampador@gmail.com>
2023-06-13 07:58:20 +0800

    Wait for the thread to finish before executing the next code
    See
    https://docs.google.com/document/d/18HQKqwGGMi00LRO732nfvQbhMbMNDiV574Jq2obzplw/edit#heading=h.xfztixu6y7om

--- com.playground/src/App.java
+++ com.playground/src/App.java
@@ -4,7 +4,7 @@ class Runner1 extends Thread {
         for (int i = 0; i < 10; ++i) {
             try {
                 // this thread will sleep at 1s, making the execution slower
-                Thread.sleep(1000);
+                Thread.sleep(10);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -39,5 +39,17 @@ public class App {
         // not a parallel execution
         t1.start();
         t2.start();
+
+        // we can wait for the thread to finish using join() method
+        try {
+            // waits for t1 and t2 to finish execution
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Finished with threads...");
     }
 }
