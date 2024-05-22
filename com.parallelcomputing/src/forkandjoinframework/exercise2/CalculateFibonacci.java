3d3a0d6485ff54710794b03de9223bac70a866d1
Joliver Estampador <joliverestampador@gmail.com>
2024-05-23 07:58:17 +0800

    Fork Join's Recursive Task - Optimizing thread
    See notes here:
    https://docs.google.com/document/d/1XaKtNSC7ZqOZXidSovEVSKgvygjhO0gOYO1tmUsonsk/edit#heading=h.4o5k45sd8euj

--- com.parallelcomputing/src/forkandjoinframework/exercise2/CalculateFibonacci.java
+++ com.parallelcomputing/src/forkandjoinframework/exercise2/CalculateFibonacci.java
@@ -18,10 +18,15 @@ public class CalculateFibonacci extends RecursiveTask<Integer> {
         CalculateFibonacci cf1 = new CalculateFibonacci(num - 1);
         CalculateFibonacci cf2 = new CalculateFibonacci(num - 2);
 
-        cf1.fork();
+//        cf1.fork();
         cf2.fork();
 
-        int num1 = cf1.join();
+//        int num1 = cf1.join();
+        // (instead of fork()) compute() will allow actual thread to execute the first cf1 (reusing the actual thread)
+        // preventing actual thread to become idle while it waits for its sub-thread to finish
+        // compute() is useful for thread optimisation
+        int num1 = cf1.compute();
+        // will still become a distinct thread
         int num2 = cf2.join();
         int sum = num1 + num2;
         System.out.println(String.format("%s+%s=%s", num1, num2, sum));
