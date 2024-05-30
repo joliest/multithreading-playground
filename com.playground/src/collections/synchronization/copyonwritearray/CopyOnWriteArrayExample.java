08f1a20b383d155130f99bc05ff61c5fce233ae2
Joliver Estampador <joliverestampador@gmail.com>
2024-05-30 08:18:27 +0800

    cleanup

--- com.playground/src/collections/synchronization/copyonwritearray/CopyOnWriteArrayExample.java
+++
@@ -1,9 +0,0 @@
-package collections.synchronization.copyonwritearray;
-
-public class CopyOnWriteArrayExample {
-    public static void main(String[] args) {
-
-        ConcurrentArray concurrentArray = new ConcurrentArray();
-        concurrentArray.simulate();
-    }
-}
